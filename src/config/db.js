dafa8375490e7e8af9df8d95cbcc32712ff7407b
Thang Le <lequocthang201197@gmail.com>
2025-04-28 10:48:41 +0700

    remove sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,34 +1,7 @@
-import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import pg from "pg";
 
 dotenv.config();
-
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  logging: false, // tắt logging SQL queries
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
-
-export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("PostgreSQL connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    process.exit(1);
-  }
-};
 
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -37,5 +10,3 @@ export const pool = new pg.Pool({
       ? { rejectUnauthorized: false }
       : false,
 });
-
-export { sequelize };
